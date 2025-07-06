72d921a85ab2f9d6abd211d1d1a3a618003193f6
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-07-06 17:27:59 +1000

    new

---
+++ Activity2.py
@@ -0,0 +1,8 @@
+lower=int(input("enter a lower number"))
+upper=int(input("enter a upper number"))
+for i in range(lower,upper+1):
+    for j in range(2,i):
+        if i%j==0:
+            break
+    else:
+        print(i)
