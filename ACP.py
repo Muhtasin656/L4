0a61e65ea237db488f0705b24599777e7b190b36
Muhtasin656 <muhtasinmasrur656@gmail.com>
2025-07-19 18:17:45 +1000

    new

---
+++ ACP.py
@@ -0,0 +1,7 @@
+dn=int(input("Enter a decimal number "))
+if dn<0:
+    print("enter a decimal number")
+else:
+    br=bin(dn)
+    print(f"The binary representation of {dn} is {br} ")
+              
